c418150389b85115490bb46054e8791d40bf6b2f
Skrys <enzo.sagnelonge@etu.u-bordeaux.fr>
2015-11-20 15:14:50 +0100

    modifié :         main.cpp

--- main.cpp
+++ main.cpp
@@ -5,45 +5,44 @@
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
 #include <sstream>
+#include <ctime>
 #include "level.h"
-#include "duck.h"
-
 using namespace std;
 
-const int SCREEN_WIDTH=750;
-const int SCREEN_HEIGHT=761;
-const int SCREEN_BPP=32;
+const int ECRAN_WIDTH = 751;
+const int ECRAN_HEIGHT = 761;
+const int NB_CANARD_TO_WIN = 6;
+const int NB_CANARD_PAR_NIVEAU = 10;
 
-/*struct canard
+
+int alea(int max)
 {
-  int x;     // abscisse
-  int y;     // ordonnée
-  SDL_Surface *source;
-  SDL_Rect lecture;
-  SDL_Rect lectureFront;
-  SDL_Rect lectureBack;
-};*/
-
-
-
-
-int main()
-{
-    SDL_Surface *screen;
-    SDL_Surface *fond = SDL_LoadBMP("backGame.bmp");
-
-
-
-    SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO );
-    screen=SDL_SetVideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_BPP,SDL_HWSURFACE | SDL_DOUBLEBUF);
-    SDL_WM_SetCaption("Projet Duck Hunt",NULL);
-    SDL_FillRect(screen,&screen->clip_rect,SDL_MapRGB(screen->format,0,0,255));
-    applySurface(0,0,fond,screen,NULL);
-
-    SDL_Flip(screen);
-    pause();
-
-    SDL_FreeSurface(screen);
-
+    return 1 + (int) (1.0*max * rand() / RAND_MAX);
 }
 
+
+
+int main(int argc, char* argv[])
+{
+     niveau n;
+     int difficulte = 0;
+
+     initNiveau(n,difficulte);
+
+      SDL_Surface *canard1;
+
+     return EXIT_SUCCESS;
+}
+
+
+/****************** Nom de la fonction **********************
+* NOM_FONCTION                                              *
+******************** Auteur , Dates *************************
+* Nom/Date : Éventuellement la version                      *
+********************* Description ***************************
+* Vous décrivez ici ce que fait cette fonction              *
+*********************** Entrées *****************************
+* Vous décrivez ici les données en entrée de la fonction    *
+*********************** Sorties *****************************
+* Vous détaillez ici ce que renvoie la fonction             *
+************************************************************/
